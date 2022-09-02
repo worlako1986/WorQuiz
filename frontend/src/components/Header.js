ad2496e15265a813571f6f0aa6cf05cd57187c2a
Worlako Dzokoto <worlako1986@hotmail.com>
2022-09-02 16:31:24 +0000

    Added image

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import '../stylesheets/Header.css';
+import React, { Component } from "react";
+import "../stylesheets/Header.css";
 
 class Header extends Component {
   navTo(uri) {
@@ -8,31 +8,31 @@ class Header extends Component {
 
   render() {
     return (
-      <div className='App-header'>
+      <div className="App-header">
         <h1
           onClick={() => {
-            this.navTo('');
+            this.navTo("");
           }}
         >
-          Udacitrivia
+          WorQuiz
         </h1>
         <h2
           onClick={() => {
-            this.navTo('');
+            this.navTo("");
           }}
         >
           List
         </h2>
         <h2
           onClick={() => {
-            this.navTo('/add');
+            this.navTo("/add");
           }}
         >
           Add
         </h2>
         <h2
           onClick={() => {
-            this.navTo('/play');
+            this.navTo("/play");
           }}
         >
           Play
